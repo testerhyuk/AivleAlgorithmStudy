a1da95a8f049a0bfba851ef7fd943fcfbcf33eed
hyuk <testerhyuk214@gmail.com>
2022-02-21 22:14:40 +0900

    백준1652

--- 구현/백준 1652번 - 누울 자리를 찾아라(브론즈1)/김규혁/백준1652.py
+++ 구현/백준 1652번 - 누울 자리를 찾아라(브론즈1)/김규혁/백준1652.py
@@ -1,8 +1,36 @@
 import sys
-sys.stdin = open("input.txt", 'r')
+sys.stdin = open('input.txt', 'r')
 
 n = int(input())
 
 room = [input() for i in range(n)]
+row, col = 0, 0
 
-print(room)
+for i in range(n):
+    row_seat, col_seat = 0, 0
+    for j in range(n):
+        # 가로
+        if room[i][j] == '.':
+            row_seat += 1
+        else:
+            if row_seat >= 2:
+                row += 1
+            # 초기화
+            row_seat = 0
+
+        # 세로
+        if room[j][i] == '.':
+            col_seat += 1
+        else:
+            if col_seat >= 2:
+                col += 1
+            # 초기화
+            col_seat = 0
+
+    if row_seat >= 2:
+        row += 1
+    
+    if col_seat >= 2:
+        col += 1
+
+print(row, col)
